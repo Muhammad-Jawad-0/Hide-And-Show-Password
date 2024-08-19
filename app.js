f8127b1f0050b7cc5e385026263db8cbd1b14dd1
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-08-19 20:48:23 +0500

    something is update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const password = document.getElementById("password");
 const eyeIcon = document.getElementById("eyeIcon");
 
 eyeIcon.addEventListener("click", () => {
-  if (password.type == "password") {
+  if (password.type == "password" && !password.value == "") {
     password.type = "text";
     eyeIcon.src = "./images/eye-open.png"
   } else {
